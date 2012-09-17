c6d2bf0b50f0747dcf8288c39afd41b35fe1c190
Федосов Сергей <sfedosov@iteq.ru>
2012-09-17 09:19:15 +0400

    autologin if have back_url

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -25,7 +25,10 @@ class AccountController < ApplicationController
   # Login request and validation
   def login
     if request.get?
-      logout_user
+	  if User.current.logged? and params.include? "back_url"
+		redirect_to back_url
+	  else
+		logout_user
     else
       authenticate_user
     end
